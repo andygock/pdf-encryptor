2c3281633bf13c5a7f4d40577ce33ca14b2a5d7e
Andy Gock <andy@gock.net>
2021-11-20 14:33:43 +1100

    Add timeout for processing, convert to promises

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-if (module.hot) {
-  module.hot.accept();
-}
-
 // https://stackoverflow.com/questions/9267899/arraybuffer-to-base64-encoded-string
 const _arrayBufferToBase64 = (buffer) => {
   let binary = "";
@@ -26,62 +22,101 @@ const downloadPDF = (filename, arrayBuffer) => {
   document.body.removeChild(element);
 };
 
-const form = document.getElementById("file");
-form.addEventListener(
-  "change",
-  (ev) => {
-    if (
-      !window.File ||
-      !window.FileReader ||
-      !window.FileList ||
-      !window.Blob
-    ) {
-      alert("The File APIs are not fully supported in this browser.");
+const handleFileInput = (ev) => {
+  if (!window.File || !window.FileReader || !window.FileList || !window.Blob) {
+    alert("The File APIs are not fully supported in this browser.");
+    return;
+  }
+
+  // get user password
+  const password = document.getElementById("password").value;
+
+  if (password.length === 0) {
+    alert("Password missing");
+    return;
+  }
+
+  // default output filename if error occurs, however output should be "Encrypted original.pdf"
+  let uploadFilename = "output.pdf";
+
+  // https://www.javascripture.com/FileReader
+  const reader = new FileReader();
+  reader.onload = async (e) => {
+    const arrayBuffer = e.target.result;
+    const array = new Uint8Array(arrayBuffer);
+
+    message.innerText = "Processing now. Please wait...";
+
+    try {
+      const encryptedArrayBuffer = await encryptPDF(array, password);
+
+      // processing complete
+      message.innerText = "";
+
+      // give user a download dialog
+      downloadPDF(`Encrypted ${uploadFilename}`, encryptedArrayBuffer);
+    } catch (err) {
+      message.innerText = "Error occured.";
+      alert(err);
       return;
     }
+  };
 
-    // get user password
-    const password = document.getElementById("password").value;
+  if (ev.target.files.length !== 1) {
+    // user uploaded too many files
+    alert("More than one file was selected. Please select one file only.");
+  } else if (ev.target.files[0].type === "application/pdf") {
+    uploadFilename = ev.target.files[0].name;
+    reader.readAsArrayBuffer(ev.target.files[0]);
+  } else {
+    alert("Selected file does not appear to be a PDF");
+  }
+};
 
-    // default output filename if error occurs, however output should be "Encrypted original.pdf"
-    let uploadFilename = "output.pdf";
+// https://github.com/j3k0/qpdf.js
+const encryptPDF = (inputArrayBuffer, password) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(new Error("Timeout exceeded"));
+    }, 10000);
 
-    // https://www.javascripture.com/FileReader
+    QPDF.encrypt({
+      logger: (text) => {
+        console.log(text);
+      },
+      arrayBuffer: inputArrayBuffer,
+      userPassword: password,
+      ownerPassword: password,
+      callback: (err, arrayBuffer) => {
+        console.log("QPDF callback()");
 
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      const arrayBuffer = e.target.result;
-      const array = new Uint8Array(arrayBuffer);
+        // note: there isn't a way to cancel the processing
+        if (err) {
+          reject(err);
+        } else if (arrayBuffer === null) {
+          reject(new Error("Unknown error occured"));
+        } else {
+          console.log("resolve()", arrayBuffer);
 
-      // https://github.com/j3k0/qpdf.js
-      QPDF.encrypt({
-        logger: (text) => {
-          // console.log(text);
-        },
-        arrayBuffer: array,
-        userPassword: password,
-        ownerPassword: password,
-        callback: (err, arrayBuffer) => {
-          if (err) {
-            alert(err.message);
-          } else if (arrayBuffer === null) {
-            alert("Unknown error occured");
-          } else {
-            downloadPDF(`Encrypted ${uploadFilename}`, arrayBuffer);
-          }
-        },
-      });
-    };
+          resolve(arrayBuffer);
+        }
+      },
+    });
+  });
+};
 
-    if (ev.target.files.length !== 1) {
-      // user uploaded too many files
-      alert("More than one file was selected. Please select one file only.");
-    } else if (ev.target.files[0].type === "application/pdf") {
-      uploadFilename = ev.target.files[0].name;
-      reader.readAsArrayBuffer(ev.target.files[0]);
-    } else {
-      alert("Selected file does not appear to be a PDF");
-    }
-  },
-  false
-);
+//
+// SCRIPT STARTS HERE
+//
+
+const message = document.getElementById("message");
+const form = document.getElementById("file");
+
+if (module.hot) {
+  module.hot.dispose(() => {
+    form.removeEventListener("change", handleFileInput);
+  });
+  module.hot.accept();
+}
+
+form.addEventListener("change", handleFileInput, false);
